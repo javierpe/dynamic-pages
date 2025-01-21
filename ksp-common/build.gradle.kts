9abe98b59943e7e57a2e1ba6f9564e9fe523f90f
Francisco Peña <francisco.javier.p.ramos@gmail.com>
2025-01-21 16:28:53 -0600

    Maven

--- ksp-common/build.gradle.kts
+++ ksp-common/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.ksp)
@@ -18,4 +20,36 @@ kotlin {
     }
 }
 
-apply(from = file("../gradle/publish.gradle.kts"))
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    coordinates("dynamic-pages", "common", "1.0.0")
+
+    pom {
+        name = "Dynamic Pages Common"
+        description = "Dynamic Pages Common"
+        inceptionYear = "2025"
+        url = "https://github.com/javierpe/dynamic-pages"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "javierpe"
+                name = "Francisco Peña"
+                url = "https://github.com/javierpe/"
+            }
+        }
+        scm {
+            url = "https://github.com/javierpe/dynamic-pages"
+            connection = "scm:git:git://github.com/javierpe/dynamic-pages"
+            developerConnection = "scm:git:ssh://git@github.com/javierpe/dynamic-pages.git"
+        }
+    }
+}
