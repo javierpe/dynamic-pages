049d3ccb646e979075a6577ae5c7db14da10424a
Adán Castillo <adan.gil97@gmail.com>
2025-01-21 11:29:08 -0600

    Configure jvm only

--- composeApp/src/commonMain/kotlin/com/nucu/dynamicpages/App.kt
+++ composeApp/src/commonMain/kotlin/com/nucu/dynamicpages/App.kt
@@ -1,22 +1,23 @@
 package com.nucu.dynamicpages
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.nucu.dynamicpages.test.rule.MainModule
 import com.nucu.dynamicpages.test.rule.NameRule
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import dynamicpages.composeapp.generated.resources.Res
-import dynamicpages.composeapp.generated.resources.compose_multiplatform
 import org.koin.compose.KoinApplication
 import org.koin.compose.koinInject
 import org.koin.ksp.generated.defaultModule
@@ -53,7 +54,6 @@ fun AppContent(modifier: Modifier = Modifier) {
                 greeting.value = rule.map("Hello")
             }
             Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                Image(painterResource(Res.drawable.compose_multiplatform), null)
                 Text("Compose: ${greeting.value}")
             }
         }
